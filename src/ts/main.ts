f8e96d4db3b175766d2e8a9eabe68c18c8a5d45e
wilmawiker <wikerwilma@gmail.com>
2023-02-06 11:53:45 +0100

    more design

---
+++ src/ts/main.ts
@@ -0,0 +1,23 @@
+let mybutton: HTMLButtonElement = document.getElementById("btn-back-to-top") as HTMLButtonElement;
+
+window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
