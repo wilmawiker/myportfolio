2e86ae8f21707b8f7be591bab337d77176ca5881
wilmawiker <wikerwilma@gmail.com>
2023-08-24 12:40:55 +0200

    added restaurant group assignment to repos

--- src/data/repos/repos.ts
+++ src/data/repos/repos.ts
@@ -2,12 +2,37 @@ import { Repo } from "../../ts/models/Repo";
 
 export let repos: Repo[] = [
   new Repo(
-    1,
-    "Todo list",
-    "One of our JavaScript assignments from school, making a todo-list that allows the user to create tasks and being able to mark them as done, undone or delete them. The tasks are stored in local storage and the user can also sort them by the date they were made.",
-    "https://dl.dropboxusercontent.com/s/gtnbctkytxoqaij/todo.png?dl=0",
-    "https://dl.dropboxusercontent.com/s/mgrfq8xbh82wg81/todomo.png?dl=0",
-    "https://github.com/wilmawiker/to-do-list"
+    7,
+    "The restaurant",
+    `Group assignment in the course 'JavaScript Extended', made with React. This project were made together with Adam Danielsson, Oskar Sterner and Ida Lindgren. We made a restaurants booking system. Were the admin also can log in to see, edit and remove bookings.`,
+    "https://dl.dropboxusercontent.com/scl/fi/xlosn5qoobrw39bpuz8ds/therestaurant.png?rlkey=wsj2la7zi5m28kyikycmcfui5&dl=0",
+    "",
+    "https://github.com/wilmawiker/therestaurant-assignment"
+  ),
+  new Repo(
+    5,
+    "The Zoo",
+    `Another assignment in the course 'JavaScript Extended', made with React. A web page where you can take care of the animals at the zoo. Animals will be hungry if they have not been fed within the last 4 hours. In that case they will show up with a red card, when fed they will have a green card.`,
+    "https://dl.dropboxusercontent.com/scl/fi/4gtqgcyaadx9t593t5gdl/thezoo.png?rlkey=id9wocbtek9tme1eazx2ojrdv&dl=0",
+    "",
+    "https://github.com/wilmawiker/thezoo-react-assignment"
+  ),
+  
+  new Repo(
+    4,
+    "Tic Tac Toe",
+    `Assignment in the course 'JavaScript Extended', a tic tac toe game made with Vue.`,
+    "https://dl.dropboxusercontent.com/scl/fi/foivctuvwvoy0bhty5tjf/tictactoe.png?rlkey=yr3ebfcm6a9cxyc4dzv7gy641&dl=0",
+    "",
+    "https://github.com/wilmawiker/tictactoe-vue-assignment"
+  ),
+  new Repo(
+    6,
+    "Code smells",
+    `Assignment done in the course Work methodology for developers. Code was delivered with a lot of bugs which we were to fix. Checklist of fixes and improvements done is in the read me file.`,
+    "https://dl.dropboxusercontent.com/scl/fi/t1inovzql9aeoze60mkws/codesmells.png?rlkey=2uiyrmtjgpo16alqjvl7mh9kn&dl=0",
+    "",
+    "https://github.com/wilmawiker/codesmells-assignment"
   ),
   new Repo(
     2,
@@ -26,27 +51,11 @@ export let repos: Repo[] = [
     "https://github.com/Ayngie/assignment-typescript-Webshop-DukafintAB"
   ),
   new Repo(
-    4,
-    "Tic Tac Toe",
-    `Assignment in the course 'JavaScript Extended', a tic tac toe game made with Vue.`,
-    "https://dl.dropboxusercontent.com/scl/fi/foivctuvwvoy0bhty5tjf/tictactoe.png?rlkey=yr3ebfcm6a9cxyc4dzv7gy641&dl=0",
-    "",
-    "https://github.com/wilmawiker/tictactoe-vue-assignment"
-  ),
-  new Repo(
-    5,
-    "The Zoo",
-    `Another assignment in the course 'JavaScript Extended', made with React. A web page where you can take care of the animals at the zoo. Animals will be hungry if they have not been fed within the last 4 hours. In that case they will show up with a red card, when fed they will have a green card.`,
-    "https://dl.dropboxusercontent.com/scl/fi/4gtqgcyaadx9t593t5gdl/thezoo.png?rlkey=id9wocbtek9tme1eazx2ojrdv&dl=0",
-    "",
-    "https://github.com/wilmawiker/thezoo-react-assignment"
-  ),
-  new Repo(
-    6,
-    "Code smells",
-    `Assignment done in the course Work methodology for developers. Code was delivered with a lot of bugs which we were to fix. Checklist of fixes and improvements done is in the read me file.`,
-    "https://dl.dropboxusercontent.com/scl/fi/t1inovzql9aeoze60mkws/codesmells.png?rlkey=2uiyrmtjgpo16alqjvl7mh9kn&dl=0",
-    "",
-    "https://github.com/wilmawiker/codesmells-assignment"
+    1,
+    "Todo list",
+    "One of our JavaScript assignments from school, making a todo-list that allows the user to create tasks and being able to mark them as done, undone or delete them. The tasks are stored in local storage and the user can also sort them by the date they were made.",
+    "https://dl.dropboxusercontent.com/s/gtnbctkytxoqaij/todo.png?dl=0",
+    "https://dl.dropboxusercontent.com/s/mgrfq8xbh82wg81/todomo.png?dl=0",
+    "https://github.com/wilmawiker/to-do-list"
   ),
 ];
